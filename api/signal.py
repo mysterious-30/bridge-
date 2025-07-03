5f89daed349dfa65bb2ee61026ac336ee5aa8d04
mysterious-30 <95312581+mysterious-30@users.noreply.github.com>
2025-07-03 18:52:34 +0530

    Delete api directory

--- api/signal.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def handler(request):
-    signal = random.choice(["BUY", "SELL", "WAIT"])
-    return {
-        "statusCode": 200,
-        "headers": { "Content-Type": "application/json" },
-        "body": f'{{"signal": "{signal}"}}'
-    }
