b29774a031b19824840ddf84765f76b36fbcd220
mysterious-30 <95312581+mysterious-30@users.noreply.github.com>
2025-07-03 18:47:25 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,6 @@
 def handler(request):
-    ip = request.get("headers", {}).get("x-forwarded-for", "unknown")
-    print(f"👋 Visitor from IP: {ip}")
-
     return {
         "statusCode": 200,
         "headers": { "Content-Type": "text/plain" },
-        "body": f"Hello from Vercel Python API! Your IP: {ip}"
+        "body": "Hello from Vercel Python!"
     }
