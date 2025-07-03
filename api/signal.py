8c04da1790e9303fbcab1c6b7f126b1fc7df9a31
mysterious-30 <95312581+mysterious-30@users.noreply.github.com>
2025-07-03 18:47:42 +0530

    Update signal.py

--- api/signal.py
+++ api/signal.py
@@ -1,11 +1,7 @@
-# api/signal.py
-
 import random
 
 def handler(request):
     signal = random.choice(["BUY", "SELL", "WAIT"])
-    print(f"📡 Signal sent: {signal}")
-
     return {
         "statusCode": 200,
         "headers": { "Content-Type": "application/json" },
